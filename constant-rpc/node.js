19b7b5f9e68b4c40fe71cb6299cdbc78eb40b350
Duc Hung <hung.han@notabasement.com>
2019-01-24 14:52:03 +0700

    get connection count + get all peers

--- constant-rpc/node.js
+++ constant-rpc/node.js
@@ -1,8 +1,26 @@
-var ConstantNodeRPC = require('./constant')
+var ConstantNodeRPC = require('./constant_rpc')
 
 ConstantNodeRPC.prototype.GetNetworkInfo = async function (params) {
   return new Promise(resolve => {
     this.client.request('getnetworkinfo', params, function (err, response) {
+      if (err) throw err
+      resolve(response.Result)
+    })
+  })
+}
+// GET CONNECTION COUNT
+ConstantNodeRPC.prototype.GetConnectionCount = async function (params) {
+  return new Promise(resolve => {
+    this.client.request('getconnectioncount', params, function (err, response) {
+      if (err) throw err
+      resolve(response.Result)
+    })
+  })
+}
+// GET ALL PEER
+ConstantNodeRPC.prototype.GetAllPeers = async function (params) {
+  return new Promise(resolve => {
+    this.client.request('getallpeers', params, function (err, response) {
       if (err) throw err
       resolve(response.Result)
     })
