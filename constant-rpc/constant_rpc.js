59920c3f8ec33926acc81339715a514a30311936
DungVan <vvstdung89@gmail.com>
2019-01-24 15:37:34 +0700

    Auto stash before merge of "master" and "origin/master"

--- constant-rpc/constant_rpc.js
+++ constant-rpc/constant_rpc.js
@@ -4,7 +4,39 @@ class ConstantNodeRPC {
   constructor (host, port) {
     this.client = RPCClient(host, port)
   }
+  //virtual method 
+  //node
+  GetNetworkInfo(){}
+  GetConnectionCount(){}
+  GetAllPeers(){}
+  GetRawMempool(){}
+  GetMempoolEntry(){}
+  EstimateFee(){}
+  GetGenerate(){}
+  GetMiningInfo(){}
+
+  //block
+  //transaction
+  ListOutputCoins(privateKey = ""){}
+  
+}
+
+// Implement virtual method
+function rpc(method, client, params){
+  return new Promise(resolve => {
+    client.request(method, params, function (err, response) {
+      if (err) throw err
+      resolve(response.Result)
+    })
+  })
+}
+
+for (let f of Object.getOwnPropertyNames( ConstantNodeRPC.prototype )) {
+  if (f == "constructor") continue
+  ConstantNodeRPC.prototype[f] = function(...params){
+    return rpc(f.toLowerCase(), this.client, params)
+  }
 }
 
 module.exports = ConstantNodeRPC
-require('./node')
+
--- tests/demo.js
+++ tests/demo.js
@@ -1,7 +1,10 @@
-const ConstantRPC = require("../constant-rpc/constant_rpc")
+const ConstantRPC = require('../constant-rpc/constant_rpc')
 
-!async function(){
-    const node1 = new ConstantRPC()
-    let res = await node1.GetNetworkInfo()
-    console.log(res)
-}()
+!(async function () {
+  const node1 = new ConstantRPC()
+  let res = await node1.GetNetworkInfo()
+//   let res = await node1.ListOutputCoins(
+//     '112t8rqnMrtPkJ4YWzXfG82pd9vCe2jvWGxqwniPM5y4hnimki6LcVNfXxN911ViJS8arTozjH4rTpfaGo5i1KKcG1ayjiMsa4E3nABGAqQh'
+//   )
+  console.log(res)
+})()
