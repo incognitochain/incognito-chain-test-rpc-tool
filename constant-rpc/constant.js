bdfa3760cf67f3e19ebca1ddf32b4455118d4701
DungVan <vvstdung89@gmail.com>
2019-01-24 13:25:08 +0700

    Update

--- constant-rpc/constant.js
+++ constant-rpc/constant.js
@@ -4,16 +4,15 @@ class Client {
   constructor (host, port) {
     this.client = RPCClient(host, port)
   }
+}
 
-  async GetNetworkInfo (params = []) {
-    return new Promise(resolve => {
-      this.client.request('getnetworkinfo', params, function (err, response) {
-        if (err) throw err
-        resolve(response.Result)
-      })
+Client.prototype.GetNetworkInfo = async function(params){
+  return new Promise(resolve => {
+    this.client.request('getnetworkinfo', params, function (err, response) {
+      if (err) throw err
+      resolve(response.Result)
     })
-  }
-  
+  })
 }
 
 module.exports = Client
