007c9773dc9d19255649303210e121ba34785c6a
DungVan <vvstdung89@gmail.com>
2019-01-29 17:30:16 +0700

    update

---
+++ tests/beacon_beststate.js
@@ -0,0 +1,15 @@
+const ConstantRPC = require('../constant-rpc/constant_rpc')
+
+!(async function () {
+    const shard = new ConstantRPC("127.0.0.1", 9334);
+    const beacon = new ConstantRPC("127.0.0.1", 9344);
+    let beaconBestState_beacon = await beacon.GetBeaconBestState();   
+    console.log("Beacon Beststate on Beacon")
+    console.log(JSON.stringify(beaconBestState_beacon, null, 4))
+
+
+    let beaconBestState_shard = await shard.GetBeaconBestState();   
+    console.log("Beacon Beststate on Shard")
+    console.log(JSON.stringify(beaconBestState_shard, null, 4))
+})()
+  
