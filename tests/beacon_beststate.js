c2f060d54bb23c7dff5939b8ecd59a4cd5cf48bd
0xmerman <hung@autonomous.nyc>
2019-02-19 15:18:33 +0700

    refactor code

--- tests/beacon_beststate.js
+++ tests/beacon_beststate.js
@@ -5,11 +5,11 @@ const ConstantRPC = require('../constant-rpc/constant_rpc')
     const beacon = new ConstantRPC("127.0.0.1", 9337);
     let beaconBestState_beacon = await beacon.GetBeaconBestState();   
     console.log("Beacon Beststate on Beacon")
-    console.log(JSON.stringify(beaconBestState_beacon, null, 4))
+    console.log(JSON.stringify(beaconBestState_beacon.Response.Result, null, 4))
 
 
     let beaconBestState_shard = await shard.GetBeaconBestState();   
     console.log("Beacon Beststate on Shard")
-    console.log(JSON.stringify(beaconBestState_shard, null, 4))
+    console.log(JSON.stringify(beaconBestState_shard.Reponse.Result, null, 4))
 })()
   
--- tests/test_getTxById.js
+++ tests/test_getTxById.js
@@ -2,6 +2,6 @@ const ConstantRPC = require('../constant-rpc/constant_rpc')
 const shard = new ConstantRPC("127.0.0.1", 9336);
 !(async function () {
     const txResult = await shard.GetTransactionByHash("ecda1414556972d6a0c8b20090100bc22fe7e2fabd86a64aaf708283183953db")
-    console.log("Result Staking Transaction", txResult);
+    console.log("Result Staking Transaction", txResult.Response.Result);
 })()
 
