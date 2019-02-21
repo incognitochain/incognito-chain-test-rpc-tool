8ff5da2e5a50b4d05a143f54adcf52cfa54e8826
--global <hung@autonomous.nyc>
2019-02-21 12:27:54 +0700

    add test staking flow

--- tests/test_getCandidate.js
+++ tests/test_getCandidate.js
@@ -1,10 +1,13 @@
 const ConstantRPC = require('../constant-rpc/constant_rpc')
 const shard = new ConstantRPC("127.0.0.1", 9334);
 !(async function () {
-    const candidate = await shard.GetCandidate()
+    const candidate = await shard.GetCandidateList()
     console.log("Result Candidate", candidate.Response.Result);
 
-    const committee = await shard.GetCommittee()
+    const committee = await shard.GetCommitteeList()
     console.log("Result Committee", committee.Response.Result);
+
+    const canStake = await shard.CanPubkeyStake("18BRApfoGh91y3WdeK9BCW1utunNLVnRCAUFvsRpSWfmfoPzPqa")
+    console.log("Result canStake", canStake.Response.Result);
 })()
 
